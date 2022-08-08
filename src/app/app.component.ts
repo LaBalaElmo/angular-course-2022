fae81e834b61ab7882b63e79f10b1ea97636d8c7
Rodrigo Tordoya <rtordoya19@isc.lp.upb.edu>
2022-08-08 17:33:42 -0400

    [08-08-2022 task 1] operator splice, reduce, json operators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test';
+
+  constructor() {
+    //operator Splice
+    const A = ['a', 'b', 'c', 'd', 'e',];
+    const auxSplice = A.splice(1, 1)
+    console.log('auxSplice: ', A, auxSplice);
+
+    //Operator Reduce
+    const auxReduce = [1, 2, 3, 4, 5].reduce((prev, value, index) => prev + value, 0)
+    console.log('auxReduce: ', auxReduce)
+
+    //JSON Operators
+    const auxJSON = {
+      id: 1,
+      name: 'JUAN',
+      lastName : 'GUTIERREZ'
+    };
+
+    //ENTRIES, KEY, VALUES
+
+    const  auxEntries = Object.entries(auxJSON);
+    console.log('auxEntries: ', auxEntries);
+
+    const auxKeys = Object.keys(auxJSON);
+    console.log('auxKeys: ', auxKeys);
+
+    const auxValues = Object.values(auxJSON);
+    console.log('auxValues: ', auxValues)
+  }
 }
