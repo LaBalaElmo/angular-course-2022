d9e55a196f76643ac0e90776cc17483578fd9d4c
Rodrigo Tordoya <rtordoya19@isc.lp.upb.edu>
2022-08-10 17:17:51 -0400

    [10-08-2022 task 1] interaction between child and parent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import {IPerson} from "./person/person.model";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'test';
+  public person?: IPerson;
+
+  getPrint(event: IPerson) {
+    console.log(event)
+    this.person = event;
+  }
 }
---
+++ src/app/person/person.component.ts
@@ -0,0 +1,30 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {IPerson} from "./person.model";
+
+@Component({
+  selector: 'app-person',
+  templateUrl: './person.component.html',
+  styleUrls: ['./person.component.scss']
+})
+export class PersonComponent implements OnInit {
+
+  @Input() name:string = '';
+  @Input() lastName:string = '';
+  @Input() age:number = 0;
+
+  @Output() print = new EventEmitter<IPerson>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  public onPrint(): void {
+    this.print.emit({
+      name: this.name,
+      lastName: this.lastName,
+      age: this.age
+    });
+  }
+
+}
