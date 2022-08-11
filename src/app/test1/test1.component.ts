d82623ad407b3f46671a8f132d70322319dbf250
Rodrigo Tordoya <rtordoya19@isc.lp.upb.edu>
2022-08-11 17:37:20 -0400

    [11-08-2022 task 1] ng On Init

---
+++ src/app/test1/test1.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test1',
+  templateUrl: './test1.component.html',
+  styleUrls: ['./test1.component.scss']
+})
+export class Test1Component implements OnInit {
+
+  constructor() {
+    console.log('constructor')
+  }
+
+  ngOnChanges(): void {
+    console.log('on changes')
+  }
+
+  ngOnInit(): void {
+    console.log('on init')
+  }
+
+}
