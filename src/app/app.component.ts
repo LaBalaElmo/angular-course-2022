48e17ee8d88f28b90a7c4dbebe6f6b01884dbd67
Rodrigo Tordoya <rtordoya19@isc.lp.upb.edu>
2022-08-15 18:38:54 -0400

    [15-08-2022 task 2] Behaviour Subject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {of} from "rxjs";
+import {BehaviorSubject, of, Subscription} from "rxjs";
 import {map} from 'rxjs/operators'
 
 @Component({
@@ -12,9 +12,49 @@ export class AppComponent {
 
   public data$ = of("hola")
 
+
+  public youtube$ = new BehaviorSubject<number>(0);
+  public tiktok$ = new BehaviorSubject<number>(0);
+  public twitter$ = new BehaviorSubject<number>(0);
+
+  public person1!: Subscription;
+  public person2!: Subscription;
+
   constructor() {
     this.data$.pipe(map( d => d+ ' map')).subscribe(res => {
       console.log('data: ', res)
     });
+
+    this.youtube$.subscribe(res => {
+      console.log('youtube subscription: ', res)
+    })
+
+    this.person1 = this.tiktok$.subscribe(res => {
+      console.log('person1 tiktok subscription: ', res)
+    })
+
+    this.person2 = this.tiktok$.subscribe(res => {
+      console.log('person2 tiktok subscription: ', res)
+    })
+
+    this.twitter$.subscribe(res => {
+      console.log('twitter subscription: ', res)
+    })
+  }
+
+  addVideo(){
+    this.youtube$.next(1);
+  }
+
+  addTiktok(){
+    this.tiktok$.next(2);
+  }
+
+  addTwit(){
+    this.twitter$.next(3)
+  }
+
+  deleteTiktok(){
+    this.person1.unsubscribe()
   }
 }
