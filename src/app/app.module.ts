1d38d3b413d3742f1ca7c0e42e13b9fb4a64d992
Rodrigo Tordoya <rtordoya19@isc.lp.upb.edu>
2022-08-16 17:29:11 -0400

    [16-08-2022 task 1] own structural directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UpbDirective } from './upb.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UpbDirective
   ],
   imports: [
     BrowserModule,
