9a926bd310a4884eb1a1e5bdeb8e0d4ac99334e9
Rodrigo Tordoya <rtordoya19@isc.lp.upb.edu>
2022-08-16 17:58:53 -0400

    [16-08-2022 task 2] Intercept input in component with setters

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UpbDirective } from './upb.directive';
+import {FormsModule} from "@angular/forms";
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UpbDirective
+    UpbDirective,
+    TestComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/upb.directive.ts
+++ src/app/upb.directive.ts
@@ -1,9 +1,9 @@
-import {Directive, Input, OnInit, TemplateRef, ViewContainerRef} from '@angular/core';
+import {Directive, Input, OnChanges, OnInit, SimpleChanges, TemplateRef, ViewContainerRef} from '@angular/core';
 
 @Directive({
   selector: '[upb]'
 })
-export class UpbDirective implements OnInit{
+export class UpbDirective implements OnInit,OnChanges{
 
   @Input() upb: boolean = true;
 
@@ -13,7 +13,15 @@ export class UpbDirective implements OnInit{
   ) {}
 
   ngOnInit(){
-    if(this.upb){
+    /*if(this.upb){
+      this.viewContainer.createEmbeddedView(this.templateRef)
+    }else {
+      this.viewContainer.clear()
+    }*/
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if(changes['upb'] && changes['upb'].currentValue){
       this.viewContainer.createEmbeddedView(this.templateRef)
     }else {
       this.viewContainer.clear()
