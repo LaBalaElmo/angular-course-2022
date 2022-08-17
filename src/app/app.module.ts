64aa2eb842292aa696b05c7295f07eb50f53a88b
Rodrigo Tordoya <rtordoya19@isc.lp.upb.edu>
2022-08-17 17:57:40 -0400

    [17-08-2022 task 2] inputs in attribute directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OverUpbDirective } from './over-upb.directive';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { OverUpbDirective } from './over-upb.directive';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/over-upb.directive.ts
+++ src/app/over-upb.directive.ts
@@ -1,12 +1,19 @@
-import {Directive, ElementRef} from '@angular/core';
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[overUpb]'
 })
-export class OverUpbDirective {
+export class OverUpbDirective implements OnInit {
+
+  @Input() overUpb!: string;
 
   constructor(private element: ElementRef) {
-    this.element.nativeElement.style.backgroundColor = 'red';
+
+  }
+
+  ngOnInit(){
+    console.log(this.element)
+    this.element.nativeElement.style.backgroundColor = this.overUpb;
   }
 
 }
