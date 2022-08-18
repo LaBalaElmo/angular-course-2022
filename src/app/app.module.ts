e4e748df3e7227c9abdbbedad4c6ff6dc2424914
Rodrigo Tordoya <rtordoya19@isc.lp.upb.edu>
2022-08-18 17:51:08 -0400

    [18-08-2022 task 2] basic modules in angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Comp1Component } from './comp1/comp1.component';
+import { Comp2Component } from './comp2/comp2.component';
+import { Comp3Component } from './comp3/comp3.component';
+import {HomeModule} from "./home/home.module";
+import {UserModule} from "./user/user.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HomeModule,
+    UserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
