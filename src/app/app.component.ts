79f576aa498d60e1eaeda208858e58e8198dcf96
Rodrigo Tordoya <rtordoya19@isc.lp.upb.edu>
2022-08-23 17:52:58 -0400

    [23-08-2022 task 2] custom impure pipe and pure pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test';
+  data = [1,2,3,4,5,6,7,8,9]
 
   constructor() {
     console.log('-----------------PURE')
