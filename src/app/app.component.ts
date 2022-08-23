593e80f795fdb67a164fd85c5bd0c811244df250
Rodrigo Tordoya <rtordoya19@isc.lp.upb.edu>
2022-08-23 18:44:17 -0400

    [23-08-2022 task 3] basic router router, routerLink

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -9,7 +10,7 @@ export class AppComponent {
   title = 'test';
   data = [1,2,3,4,5,6,7,8,9]
 
-  constructor() {
+  constructor( private router: Router) {
     console.log('-----------------PURE')
     console.log(this.pureExample(3, 5))
     console.log(this.pureExample(1, 5))
@@ -29,5 +30,13 @@ export class AppComponent {
 
   impureExample(a: number, b: number): number{
     return a + b + (Math.floor(Math.random() * 10))
+  }
+
+  goToUser() {
+    this.router.navigate(['/user'])
+  }
+
+  goToHome() {
+    this.router.navigate(['/'])
   }
 }
