b9b965567aa523397935d4e362d9747918f74101
Rodrigo Tordoya <rtordoya19@isc.lp.upb.edu>
2022-09-14 19:54:36 -0400

    ultimo parcial

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  app: {
+    apiBaseUrl: 'https://test-upb-438a7-default-rtdb.firebaseio.com/'
+  },
+  auth: {
+    apiBaseUrl: 'https://identitytoolkit.googleapis.com',
+    apiKey: 'AIzaSyDKOvjSxMitFyKwxv8QTWnJdgEyVngomNM'
+  }
 };
